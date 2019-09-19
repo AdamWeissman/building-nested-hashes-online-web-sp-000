ba77a5b65382754bbcfef3c4165a02b65118ec3e
Adam Weissman <adam@comedytragedyepic.com>
2019-09-19 10:25:24 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,15 +22,7 @@ def bonus
 
   #code your solution here:
 
-  epic_tragedy.each do |level1|
-    level1.each do |level2|
-        unless :level2[:status]= "alive"
-          next
-        else 
-          :level2[:status]= "dead"
-        end
-    end  
-  end
+  epic_tragedy[:montague][:hero][:status]= "dead"
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
